a9d540eb151d9a06ff2d4dcc1c45867d504a99bb
Mayank-Vhits <mayankparmar23599@gmail.com>
2021-06-01 18:42:45 +0530

    todo app solution

---
+++ src/App.js
@@ -0,0 +1,33 @@
+// React is loaded and is available as React and ReactDOM
+// imports should NOT be used
+//todo app solution
+import React from "react";
+const TodoItem = (props) => <li onClick={props.onClick}>{props.item.text}</li>
+
+class TodoList extends React.Component {
+  render() {
+    const { items, onListClick,onItemClick } = this.props;
+    return (<ul onClick={onListClick}>
+      {items.map((item, index) => 
+                 <TodoItem item={item} key={index} onClick={this.handleItemClick.bind(this, item)}/>)}
+    </ul>);
+  }
+  
+  handleItemClick(item, event) {
+        
+          if(item.done===false){
+            this.props.onItemClick(item,event)
+          }
+        
+  }
+}
+
+
+
+const App = (props) => <TodoList
+  items={props.items}
+  onListClick={(event) => console.log("List clicked!")}
+  onItemClick={(item, event) => { console.log(item, event) }}
+/>;
+
+export default App;
